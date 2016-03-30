1070b85b168da4d57c92d9b0d5514fb74a584717
umbreen-deweb <umbreen.ahmad@gmail.com>
2016-03-30 00:40:33 -0500

    done i think

---
+++ AustinCodingAcademy_Spring2016/Week_8/Exercise/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    
+    var animatedName = 'animated shake';
+    
+    var animatedEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend' ;
+    
+    $('.my_button').click(function() {
+        
+        $('.my_shake').addClass(animatedName).one(animatedEnd, function () {
+            
+            $(this).removeClass(animatedName);
+            
+        });
+        
+    });
+    
+});
